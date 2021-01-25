1453e77adc239f59830ec3581b8c57a7cc47b822
wfng <wfngntu@gmail.com>
2021-01-25 10:17:35 +0800

    added pluralization support using Jinja2 and Babel

--- myapp_rental.py
+++ myapp_rental.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from babel.plural import PluralRule
 
 import glob
 import json
@@ -11,8 +12,9 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-app_language = 'en'
+default_fallback = 'en'
 languages = {}
+plural_rule = PluralRule({'one': 'n in 0..1'})
 
 language_list = glob.glob("languages/*.json")
 for lang in language_list:
@@ -22,12 +24,37 @@ for lang in language_list:
     with open(lang, 'r', encoding='utf8') as file:
         languages[lang_code] = json.load(file)
 
-@app.get("/rental/{language}", response_class=HTMLResponse)
-async def rental(request: Request, language: str):
-    if(language not in languages):
-        language = app_language
+
+# custom filters for Jinja2
+def plural_formatting(key_value, input, locale):
+    key = ''
+    for i in languages[locale]:
+        if(key_value == languages[locale][i]):
+            key = i
+            break
+
+    if not key:
+        return key_value
+
+    plural_key = f"{key}_plural"
+
+    if(plural_rule(input) != 'one' and plural_key in languages[locale]):
+        key = plural_key
+
+    return languages[locale][key]
+
+
+# assign filter to Jinja2
+templates.env.filters['plural_formatting'] = plural_formatting
+
+
+@app.get("/rental/{locale}", response_class=HTMLResponse)
+async def rental(request: Request, locale: str):
+    if(locale not in languages):
+        locale = default_fallback
 
     result = {"request": request}
-    result.update(languages[language])
+    result.update(languages[locale])
+    result.update({'locale': locale, 'bedroom_value': 2})
 
     return templates.TemplateResponse("index.html", result)
